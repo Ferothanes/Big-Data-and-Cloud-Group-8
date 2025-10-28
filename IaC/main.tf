b6b5793b1b50d5ec7bd0fdced169d0cf2db57208
Ferothanes <freja.kindstrand@hotmail.com>
2025-10-28 16:29:58 +0100

    deploy azure dashboard

--- IaC/main.tf
+++ IaC/main.tf
@@ -19,7 +19,7 @@ provider "azurerm" {
 # Resource Group
 # -------------------------------
 resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"   # <<< change name if you want
+  name     = "myTFResourceGroup"
   location = "swedencentral"
 }
 
@@ -35,7 +35,7 @@ resource "random_integer" "number" {
 # Storage Account for DuckDB & DBT
 # -------------------------------
 resource "azurerm_storage_account" "storage" {
-  name                     = "${var.prefix_app_name}storage${random_integer.number.result}"
+  name                     = lower("${substr(var.prefix_app_name,0,5)}st${random_integer.number.result}") # ≤24 chars
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
@@ -45,18 +45,17 @@ resource "azurerm_storage_account" "storage" {
 resource "azurerm_storage_share" "file_share" {
   name                 = "data"
   storage_account_name = azurerm_storage_account.storage.name
-  quota                = 50  # GB
+  quota                = 10 # GB
 }
-
 
 # -------------------------------
 # Azure Container Registry (ACR)
 # -------------------------------
 resource "azurerm_container_registry" "acr" {
-  name                = "${var.prefix_app_name}acr${random_integer.number.result}"
+  name                = lower("${substr(var.prefix_app_name,0,5)}acr${random_integer.number.result}") # ≤24 chars
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  sku                 = "Basic"   # <<< cheap tier
+  sku                 = "Basic"
   admin_enabled       = true
 }
 
@@ -68,7 +67,7 @@ resource "azurerm_service_plan" "asp" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
-  sku_name            = "B1"  # <<< cheap dev/test tier
+  sku_name            = "B1"
 }
 
 # -------------------------------
@@ -90,41 +89,5 @@ resource "azurerm_linux_web_app" "dashboard" {
     DOCKER_REGISTRY_SERVER_USERNAME = azurerm_container_registry.acr.admin_username
     DOCKER_REGISTRY_SERVER_PASSWORD = azurerm_container_registry.acr.admin_password
     DOCKER_CUSTOM_IMAGE_NAME        = "${azurerm_container_registry.acr.login_server}/${var.prefix_app_name}-dashboard:latest"
-  }
-}
-
-# -------------------------------
-# Container Instance (DWH Pipeline)
-# -------------------------------
-resource "azurerm_container_group" "dwh_pipeline" {
-  name                = "${var.prefix_app_name}-pipeline"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Linux"
-  ip_address_type     = "Public"
-  dns_name_label      = "${var.prefix_app_name}-pipeline-${random_integer.number.result}"
-
-  container {
-    name   = "dwh-pipeline"
-    image  = "${azurerm_container_registry.acr.login_server}/dwh_pipeline:latest"
-    cpu    = "1"
-    memory = "4"
-
-    ports {
-      port     = 80
-      protocol = "TCP"
-    }
-
-
-    environment_variables = {
-      DBT_PROFILES_DIR = "/mnt/data/.dbt"
-      DUCKDB_PATH      = "/mnt/data/job_ads.duckdb"
-    }
-  }
-
-  image_registry_credential {
-    server   = azurerm_container_registry.acr.login_server
-    username = azurerm_container_registry.acr.admin_username
-    password = azurerm_container_registry.acr.admin_password
   }
 }
